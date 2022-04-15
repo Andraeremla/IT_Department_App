52df39cbf6e1242af6df294cd9463dbf35bfef23
andraeremla <andrae.palmer@gmail.com>
2022-04-14 19:32:47 -0500

    Initial commit

---
+++ app/src/main/java/com/example/itdepartmentapp/StaffDetails.kt
@@ -0,0 +1,11 @@
+package com.example.itdepartmentapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StaffDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_staff_details)
+    }
+}
