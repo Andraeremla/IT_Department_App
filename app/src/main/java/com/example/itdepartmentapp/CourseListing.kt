fa3139637d25c025d04e913354cb66c72e9bda89
andraeremla <andrae.palmer@gmail.com>
2022-04-21 21:23:28 -0500

    Latest version of the app

--- app/src/main/java/com/example/itdepartmentapp/CourseListing.kt
+++ app/src/main/java/com/example/itdepartmentapp/CourseListing.kt
@@ -1,7 +1,9 @@
 package com.example.itdepartmentapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -10,25 +12,31 @@ import com.google.firebase.database.*
 
 
 class CourseListing : AppCompatActivity() {
-    private var helper = DatabaseHandler(this)
-
-
-    //private lateinit var dbref : DatabaseReference
-    //private lateinit var listView: ListView = findViewById(R.id.lvCoursesList)
-    //private lateinit var courseArrayList : ArrayList<courseInfo>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_course_listing)
-        helper.insertData("ITT101" ,"Computer Information Systems","3","none", "Basics in using and operating a computer system")
 
-        var listData: ArrayList<courseInfo> = helper.listOfCourses()
+        //Reads faculty list from items in strings.xml file
+        val courseList: Array<String> = resources.getStringArray(R.array.timetable)
+        //Creates adapter that sets list type and list item
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, courseList)
 
+        val listView: ListView = findViewById(R.id.lvCoursesList)
+        //ListView control populated with list items
+        listView.adapter = adapter
 
-        var listView: ListView = findViewById(R.id.lvCoursesList)
-        var listAdapter = CLAdapter(this, listData)
-        listView.adapter = listAdapter
-
+        //Defines action when list item is clicked
+        listView.setOnItemClickListener { parent, view, position, id ->
+            //Displays a message showing list item index
+           //Toast.makeText(this, "Clicked item : "+position, Toast.LENGTH_SHORT).show()
+            //Specifies which activity should be launched
+            val intent = Intent(this, CoursesView::class.java);
+            //Stores index number to be shared with Details activity
+            intent.putExtra("listIndex", position.toString());
+            //Starts activity
+            this.startActivity(intent);
+        }
 
         }
 
--- app/src/main/java/com/example/itdepartmentapp/CoursesView.kt
+++ app/src/main/java/com/example/itdepartmentapp/CoursesView.kt
@@ -2,10 +2,102 @@ package com.example.itdepartmentapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class CoursesView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_courses_view)
+
+        //Gets value sent from Directory activity
+        val index: String? = this.intent.getStringExtra("listIndex")
+
+        //Creates objects for each activity element
+        val name: TextView = findViewById(R.id.course_name)
+        val code: TextView = findViewById(R.id.tvCode)
+        val credits: TextView = findViewById(R.id.tvCredits)
+        val require: TextView = findViewById(R.id.tvRequire)
+        val details: TextView = findViewById(R.id.tvDetails)
+
+        //Assigns value to elements according to retrieved index
+        when(index){
+            "0" -> {
+                name.text = "Computer Information Systems"
+                code.text = "ITT101"
+                credits.text = "3"
+                require.text = "none"
+                details.text = "Basics in using and operating a computer system"
+            }
+            "1" -> {
+                name.text = "Discrete Mathematics 1"
+                code.text = "ITT102"
+                credits.text = "3"
+                require.text = "none"
+                details.text = "What are discrete mathematical structures? Discrete mathematical structures are also known as Decision Mathematics or Finite Mathematics. This is very popularly used in computer science for developing programming languages, software development, cryptography, algorithms etc."
+            }
+            "2" -> {
+                name.text = "Programming Techniques"
+                code.text = "ITT103"
+                credits.text = "3"
+                require.text = "ITT101"
+                details.text = "Learn the basics of programming and computer science."
+            }
+            "3" -> {
+                name.text = "Database Management Systems 1"
+                code.text = "ITT104"
+                credits.text = "3"
+                require.text = "ITT103"
+                details.text = "This covers the basics of working with databases"
+            }
+            "4" -> {
+                name.text = "Computer Essentails and Troubleshooting"
+                code.text = "ITT106"
+                credits.text = "3"
+                require.text = "ITT101"
+                details.text = "Learn how to build computer systems from scratch and how to fix them"
+            }
+            "5" -> {
+                name.text = "Object Oriented Programming Using C++ "
+                code.text = "ITT200"
+                credits.text = "3"
+                require.text = "ITT103"
+                details.text = "Learn object oriented programming concepts using the C++ language"
+            }
+            "6" -> {
+                name.text = "Data Communications & Networks"
+                code.text = "ITT201"
+                credits.text = "3"
+                require.text = "ITT101"
+                details.text = "Learn everything you need to know to build computer network systems"
+            }
+            "7" -> {
+                name.text = "Internet Authoring 1"
+                code.text = "ITT208"
+                credits.text = "3"
+                require.text = "ITT103"
+                details.text = "BLearn how to Build websites and web apps"
+            }
+            "8" -> {
+                name.text = "Data Structures and File Management 1 "
+                code.text = "ITT203"
+                credits.text = "3"
+                require.text = "ITT101"
+                details.text = "Learn how to manage datastructures in a large enterprise systems"
+            }
+            "9" -> {
+                name.text = "Systems Analysis & Design "
+                code.text = "ITT205"
+                credits.text = "3"
+                require.text = "ITT210 "
+                details.text = "Learn how to analyze and design computer systems and data structures"
+            }
+
+
+
+
+
     }
+}
+
 }
